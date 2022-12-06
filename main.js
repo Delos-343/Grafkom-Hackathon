269965598fbc9b51324942763884e739123f165c
Delos-343 <mfachry.handoko@gmail.com>
2022-12-06 14:03:49 +0700

    set up canvas scene + camera

--- main.js
+++ main.js
@@ -1,10 +1,24 @@
+'use strict'
 
-var scene;
-var box;
-var floor;
+function main() {
 
-function makeScene() {
+    // new scene
+    const canvas = document.querySelector('nekoruuNyaa');
 
-    // do something
-    
+    const renderer = new Three.WebGLRenderer({
+        canvas,
+        alpha: true,
+    });
+
+    // env setup
+    const fov = 75;
+    const aspect = 2;  // canvas default size
+    const near = 0.1;
+    const far = 5;
+
+    // add camera
+    const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
+
+    camera.position.z = 2;
+
 }
