8b4c910f83ebdece3c331f76611efd73bd9795b5
Delos-343 <mfachry.handoko@gmail.com>
2022-12-06 14:07:26 +0700

    added light and set up box

--- main.js
+++ main.js
@@ -21,4 +21,23 @@ function main() {
 
     camera.position.z = 2;
 
+    const scene = new Three.Scene();
+    {
+        const color = 0xFFFFFF;
+        const intensity = 1;
+
+        // added light
+        const light = new THREE.DirectionalLight(color, intensity);
+
+        light.position.set(-1, 2, 4);
+
+        scene.add(light);
+    }
+
+    // set up box
+    const boxWidth = 0.5;
+    const boxHeight = 0.5;
+    const boxDepth = 0.5;
+
+    const geometry = new THREE.BoxGeometry(boxWidth, boxHeight, boxDepth);
 }
